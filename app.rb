68ec10a623cad3a566a07006bba3dbd83876568c
davidfloyd91 <davidfloyd91@gmail.com>
2019-01-29 14:48:40 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
 require './environment'
+require_relative 'app/models/pirate'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    set :views, "views/pirates"
 
-  end
-end
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      params[:pirate][:ships].each do |ship_stuff|
+        Ship.new(ship_stuff)
+      end
+
+      @ship1 = Ship.all[0]
+      @ship2 = Ship.all[1]
+
+      @pirate = Pirate.new(params[:pirate])
+      erb :show
+    end
+
+  end # class
+end # module
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
 end
